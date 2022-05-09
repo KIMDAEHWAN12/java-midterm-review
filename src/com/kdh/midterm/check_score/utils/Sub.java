40a3241341d41c7d15a1dd12b893729a2a113129
KIMDAEHWAN12 <nicole171267@gmail.com>
2022-05-09 22:23:39 +0900

    Refact: 학생 평가 개선 완료

--- src/com/kdh/midterm/check_score/utils/Sub.java
+++ src/com/kdh/midterm/check_score/utils/Sub.java
@@ -16,7 +16,7 @@ public class Sub {
 		int[][] sub_dap = new int[3][10]; // 0 initialized(0d, '\0', false, ...)
 		
 		// Student Answers
-		int[][] stu_dap = new int[3][10];
+		int[][][] stu_dap = new int[3][n][10];
 	
 		// 3 kinds of state: int[][]
 		int[][] stu_ox = new int[3][n]; // CUMSUM
@@ -45,65 +45,48 @@ public class Sub {
 		}
 		
 		System.out.printf("\n %s\n", Jul);
+		
 		// 학생 N명
-		for(int j=0;j<n;j++) {
+		for(int k=0;k<n;k++) {
 			
-			// 학생 제출 답 저장
-			for(int i=0;i<10;i++) {
-				stu_dap1[i] = (int)(Math.random()*4+1);
-				stu_dap2[i] = (int)(Math.random()*4+1);
-				stu_dap3[i] = (int)(Math.random()*4+1);
+			// (이 학생의) 과목별 답안
+			for(int i = 0;i < 3; i++) {
+				for(int j = 0; j < 10; j++) {
+					// i: Subject index
+					// k: Student index
+					// j: Each answers
+					stu_dap[i][k][j] = (int)(Math.random()*4+1);
+				}
 			}
 			
-			// 시험
+			// 시험(평가)
 			System.out.printf("\n %s\n", Jul);
-			System.out.printf(" 학생%d \n", j+1);
-			o = 0; 	// 맞은 갯수 초기화
-			System.out.printf(" 과목1 ");
-			for(int i=0;i<10;i++) {
-				if(sub_dap1[i] == stu_dap1[i]) {
-					o++;
-					System.out.print(" O ");
-				} else {
-					System.out.print(" X ");
+			System.out.printf(" 학생%d \n", k+1);
+			for(int i = 0; i < 3; i++) {
+				o = 0; 	// 맞은 개수 초기화
+				System.out.printf(" 과목1 ");
+				for(int j = 0; j < 10; j++) {
+					if(sub_dap[i][j] == stu_dap[i][k][j]) {
+						o++;
+						System.out.print(" O ");
+					} else {
+						System.out.print(" X ");
+					}
+					stu_ox[i][k] = o * 10;
 				}
-				stu_ox1[j] = o*10;
-			}	
-			System.out.printf(" %3d점 ", stu_ox1[j]);
-			
-			o = 0; 	// 맞은 갯수 초기화
-			System.out.printf("\n 과목2 ");
-			for(int i=0;i<10;i++) {
-				if(sub_dap2[i] == stu_dap2[i]) {
-					o++;
-					System.out.print(" O ");
-				} else {
-					System.out.print(" X ");
-				}
-				stu_ox2[j] = o*10;
-			}	
-			System.out.printf(" %3d점 ", stu_ox2[j]);
-			
-			o = 0; 	// 맞은 갯수 초기화
-			System.out.printf("\n 과목3 ");
-			for(int i=0;i<10;i++) {
-				if(sub_dap3[i] == stu_dap3[i]) {
-					o++;
-					System.out.print(" O ");
-				} else {
-					System.out.print(" X ");
-				}
-				stu_ox3[j] = o*10;
-			}	
-			System.out.printf(" %3d점 ", stu_ox3[j]);
+				System.out.printf(" %3d점 ", stu_ox[i][k]);
+			}// for(i): i as subject index
 
 		} // 학생 N명 for
+		
 		System.out.printf("\n %s\n", Jul);
 		System.out.println(" 성명  과목1  과목2  과목3");
 		System.out.printf(" %s\n", Jul);
-		for(int i=0;i<n;i++) {
-			System.out.printf(" 학생%d %3d   %3d   %3d\n", i+1, stu_ox1[i], stu_ox2[i], stu_ox3[i]);
-		}	
+		
+		for(int k = 0; k < n; k++) {
+			System.out.printf(" 학생%d %3d   %3d   %3d\n", k+1, stu_ox[k][0], stu_ox[k][1], stu_ox[k][2]);
+		}
+		
 		System.out.printf(" %s\n", Jul);
 	}	// kor_check
-}
+}// END::public class
